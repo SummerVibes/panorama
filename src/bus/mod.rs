d2e8767e13dc4b7af22463807e24aadf6022fa76
xuebling <asxxw741@qq.com>
2021-05-06 11:12:07 +0800

    fix peer module

--- src/bus/mod.rs
+++ src/bus/mod.rs
@@ -3,6 +3,6 @@ pub mod peer;
 
 pub const BROADCAST_SEND: &str = "8888";
 pub const BROADCAST_RECV: &str = "8889";
-pub const GOSSIP_ADDRESS: &str = "0.0.0.0:9000";
+pub const GOSSIP_ADDRESS: &str = "0.0.0.0";
 pub const GOSSIP_ADDRESS_PORT: u16 = 9000;
 pub const HTTP_SERVICE_PORT: &str = "8080";
